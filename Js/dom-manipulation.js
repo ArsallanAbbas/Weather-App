798b857a1beeb157f94f8cb70bcf2dea5f54907e
Arsallan <sayedarsallan@yahoo.com>
2024-06-13 14:32:39 +0500

    Location added in dom and html

--- Js/dom-manipulation.js
+++ Js/dom-manipulation.js
@@ -1,5 +1,7 @@
 export function populateDom(obj) {
     document.getElementById("icon").setAttribute("src", `https://openweathermap.org/img/wn/${obj.weather[0].icon}.png`);
+    document.getElementById("city").innerText += obj.name;
+    document.getElementById("city").innerHTML += `<span>, ${obj.sys.country}<span/>`;
     document.getElementById("conditions").innerText += obj.weather[0].description;
     document.getElementById("temprature").innerText += obj.main.temp;
     document.getElementById("humidity").innerText += obj.main.humidity;
--- Js/script.js
+++ Js/script.js
@@ -2,11 +2,5 @@ import {currentLocationWeather} from "./current-location-weather.js"
 import { fetchCoordinates } from "./fetch-coordinates.js";
 
 document.getElementById("search-input").addEventListener("change", fetchCoordinates)
-// fetchCoordinates();
-
-
-// function searchLocationWeather() {
-
-// }
 
 currentLocationWeather();
